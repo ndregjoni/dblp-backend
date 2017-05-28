48da585243d542a8f78b542a77b3596ed4c6cd71
Fatjon Ndregjoni <extern.ndregjoni_fatjon@allianz.de>
2017-05-28 13:29:58 +0200

    initial commit

---
+++ src/main/java/com/dblp/dblpbackend/DblpBackendApplication.java
@@ -0,0 +1,12 @@
+package com.dblp.dblpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DblpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DblpBackendApplication.class, args);
+	}
+}
